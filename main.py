5214d4d73a613edbf29e4672fe8b8c111a2a6e4a
Andy Lulham <a.lulham@gmail.com>
2023-11-29 07:57:16 +0000

    Refactor a bit

--- main.py
+++ main.py
@@ -1,68 +1,76 @@
-from random import choice
+import csv
 from datetime import datetime, timezone
-import sys
 import json
+from random import choice
 import requests
+import sys
+
+
+# def closest_fraction(target_decimal, max_denominator=10):
+#     best_fraction = None
+#     best_difference = float("inf")
+#     for denominator in range(max_denominator, 0, -1):
+#         numerator = round(target_decimal * denominator)
+#         current_fraction = numerator / denominator
+#         difference = abs(target_decimal - current_fraction)
+#         if difference < best_difference:
+#             best_fraction = (numerator, denominator)
+#             best_difference = difference
+#     return best_fraction
 
 
 def run():
     r = requests.get(sys.argv[1])
     r.raise_for_status()
     data = r.json()
-    total = int(data["amountRaised"])
-    donations = int(data["donationCount"])
 
-    totaliser_size = 10
-    target_total = int(data["target"])
-
-    # start_dt = datetime.fromisoformat(data["startDate"])
     end_dt = datetime.fromisoformat(data["endDate"])
+    # start_dt = datetime.fromisoformat(data["startDate"])
+    # time_complete = 100 * (now_dt - start_dt).total_seconds() / (end_dt - start_dt).total_seconds()
 
     now_dt = datetime.now(tz=timezone.utc)
-
     if now_dt > end_dt:
         return
 
-    icon_pairs = [
-        ("large_green_square", "large_red_square"),
-        ("full_moon_with_face", "new_moon_with_face"),
-        ("smiley", "no_mouth"),
-        ("goodmaggie", "hotdog"),
-        ("green_heart", "heart"),
-        ("bell_pepper", "hot_pepper"),
-        ("hatching_chick", "egg"),
-        ("green_apple", "tomato"),
-        ("full_moon", "new_moon"),
-        ("white_check_mark", "x"),
-        ("fire", "thisisfine"),
-        ("full-fact", "fakenews"),
-        ("here", "away"),
-        ("partyblob", "thinking_face"),
-        ("star", "black_square"),
-        ("tulip", "seedling"),
-        ("butterfly", "insect"),
-        ("gratitude-thank-you", "ok"),
-        ("city_sunrise", "cityscape"),
-        ("tada", "eyes"),
-        ("partyparrot", "sleepyparrot"),
-        ("neutral_face", "dotted_line_face"),
-    ]
+    with open("icons.csv") as fh:
+        icon_pairs = list(csv.reader(fh))[1:]
     done_icon, todo_icon = choice(icon_pairs)
 
-    # time_complete = 100 * (now_dt - start_dt).total_seconds() / (end_dt - start_dt).total_seconds()
+    amount_raised = int(data["amountRaised"])
+    target = int(data["target"])
+    donation_count = int(data["donationCount"])
+    # done_total, totaliser_total = closest_fraction(amount_raised / target)
+    # todo_total = totaliser_total - done_total
+    totaliser_size = 10
+    done_total = round(totaliser_size * amount_raised / target)
+    todo_total = totaliser_size - done_total
+    totaliser = "{done}{todo}".format(
+        done=(done_total * f":{done_icon}: "),
+        todo=(todo_total * f":{todo_icon}: "),
+    ).strip()
+    average_donation = amount_raised / donation_count / 2
+    total_percent = 100 * amount_raised / target
 
-    totaliser_score = round(totaliser_size * total / target_total)
-    totaliser = (totaliser_score * f":{done_icon}: ") + ((totaliser_size - totaliser_score) * f":{todo_icon}: ")
-    message = f"£{total:,} raised so far, from {donations:,} donations. That’s an average donation of £{(total / donations / 2):.2f}.\n\nProgress to target:\n{totaliser}\n(i.e. {(100 * total / target_total):.1f}%)"
+    message = (
+        f"£{amount_raised:,} raised so far, "
+        f"from {donation_count:,} donations. "
+        f"That’s an average donation of £{average_donation:.2f}.\n\n"
+        "Progress to target:\n"
+        f"{totaliser}\n"
+        f"(i.e. {total_percent:.1f}%)"
+    )
+    print(message)
 
     with open("data/output.json") as fh:
         prev_data = json.load(fh)
+
     if prev_data["donationCount"] != data["donationCount"]:
         requests.post(
             sys.argv[2],
             json={
                 "message": message,
             })
+
     with open("data/output.json", "w") as fh:
         json.dump(data, fh)
 
